0b9956c179533cc79f5dcb8ad664a8f1568dc61d
Robert Gulewicz <robert.gulewicz@gmail.com>
2024-06-24 04:01:52 +0200

    feat: Add dynamic water shimmering

--- src/Globe/Water.tsx
+++ src/Globe/Water.tsx
@@ -1,27 +1,44 @@
-import { ReactNode } from 'react';
-import { RepeatWrapping, TextureLoader } from 'three';
-import { useLoader } from '@react-three/fiber';
+import { ReactNode, useRef } from 'react';
+import { MeshPhysicalMaterial, RepeatWrapping, TextureLoader } from 'three';
+import { useFrame, useLoader } from '@react-three/fiber';
 
 import waterMapImg from './assets/water.jpg';
+import { fragmentShader, vertexShader } from './waterShader';
 
 export function Water(): ReactNode {
   const waterMap = useLoader(TextureLoader, waterMapImg);
   waterMap.wrapS = RepeatWrapping;
   waterMap.wrapT = RepeatWrapping;
 
+  const materialRef = useRef<MeshPhysicalMaterial>(null);
+  useFrame(({ clock }) => {
+    if (!materialRef.current?.userData?.shader?.uniforms) {
+      return;
+    }
+    const elapsedTime = clock.getElapsedTime();
+    materialRef.current!.userData.shader.uniforms.uTime.value = elapsedTime;
+  });
+
   return (
     <mesh receiveShadow>
       <sphereGeometry args={[1.017, 50, 50]} />
       <meshPhysicalMaterial
+        ref={materialRef}
         color="rgb(107, 233, 255)"
-        roughnessMap={waterMap}
         ior={1.4}
         transmission={0.5}
         transparent={true}
         thickness={0.0}
         envMapIntensity={0.2}
-        metalness={0.01}
-        roughness={1.4}
+        metalness={0.0}
+        opacity={0.9}
+        roughnessMap={waterMap}
+        onBeforeCompile={(shader) => {
+          materialRef.current!.userData.shader = shader;
+          shader.uniforms.uTime = { value: 0 };
+          shader.vertexShader = vertexShader;
+          shader.fragmentShader = fragmentShader;
+        }}
       />
     </mesh>
   );
---
+++ src/Globe/waterShader.ts
@@ -0,0 +1,302 @@
+export const vertexShader = `
+#define STANDARD
+varying vec3 vViewPosition;
+#ifdef USE_TRANSMISSION
+	varying vec3 vWorldPosition;
+#endif
+#include <common>
+#include <batching_pars_vertex>
+#include <uv_pars_vertex>
+#include <displacementmap_pars_vertex>
+#include <color_pars_vertex>
+#include <fog_pars_vertex>
+#include <normal_pars_vertex>
+#include <morphtarget_pars_vertex>
+#include <skinning_pars_vertex>
+#include <shadowmap_pars_vertex>
+#include <logdepthbuf_pars_vertex>
+#include <clipping_planes_pars_vertex>
+void main() {
+	#include <uv_vertex>
+	#include <color_vertex>
+	#include <morphinstance_vertex>
+	#include <morphcolor_vertex>
+	#include <batching_vertex>
+	#include <beginnormal_vertex>
+	#include <morphnormal_vertex>
+	#include <skinbase_vertex>
+	#include <skinnormal_vertex>
+	#include <defaultnormal_vertex>
+	#include <normal_vertex>
+	#include <begin_vertex>
+	#include <morphtarget_vertex>
+	#include <skinning_vertex>
+	#include <displacementmap_vertex>
+	#include <project_vertex>
+	#include <logdepthbuf_vertex>
+	#include <clipping_planes_vertex>
+	vViewPosition = - mvPosition.xyz;
+	#include <worldpos_vertex>
+	#include <shadowmap_vertex>
+	#include <fog_vertex>
+  #ifdef USE_TRANSMISSION
+      vWorldPosition = worldPosition.xyz;
+  #endif
+  }`;
+
+export const fragmentShader = `#define STANDARD
+#ifdef PHYSICAL
+  #define IOR
+  #define USE_SPECULAR
+#endif
+uniform vec3 diffuse;
+uniform vec3 emissive;
+
+vec4 mod289(vec4 x) {
+  return x - floor(x * (1.0 / 289.0)) * 289.0; }
+
+float mod289(float x) {
+  return x - floor(x * (1.0 / 289.0)) * 289.0; }
+
+vec4 permute(vec4 x) {
+     return mod289(((x*34.0)+1.0)*x);
+}
+
+float permute(float x) {
+     return mod289(((x*34.0)+1.0)*x);
+}
+
+vec4 taylorInvSqrt(vec4 r)
+{
+  return 1.79284291400159 - 0.85373472095314 * r;
+}
+
+float taylorInvSqrt(float r)
+{
+  return 1.79284291400159 - 0.85373472095314 * r;
+}
+
+vec4 grad4(float j, vec4 ip)
+  {
+  const vec4 ones = vec4(1.0, 1.0, 1.0, -1.0);
+  vec4 p,s;
+
+  p.xyz = floor( fract (vec3(j) * ip.xyz) * 7.0) * ip.z - 1.0;
+  p.w = 1.5 - dot(abs(p.xyz), ones.xyz);
+  s = vec4(lessThan(p, vec4(0.0)));
+  p.xyz = p.xyz + (s.xyz*2.0 - 1.0) * s.www;
+
+  return p;
+  }
+
+// (sqrt(5) - 1)/4 = F4, used once below
+#define F4 0.309016994374947451
+
+float snoise(vec4 v)
+  {
+  const vec4  C = vec4( 0.138196601125011,  // (5 - sqrt(5))/20  G4
+                        0.276393202250021,  // 2 * G4
+                        0.414589803375032,  // 3 * G4
+                       -0.447213595499958); // -1 + 4 * G4
+
+// First corner
+  vec4 i  = floor(v + dot(v, vec4(F4)) );
+  vec4 x0 = v -   i + dot(i, C.xxxx);
+
+// Other corners
+
+// Rank sorting originally contributed by Bill Licea-Kane, AMD (formerly ATI)
+  vec4 i0;
+  vec3 isX = step( x0.yzw, x0.xxx );
+  vec3 isYZ = step( x0.zww, x0.yyz );
+//  i0.x = dot( isX, vec3( 1.0 ) );
+  i0.x = isX.x + isX.y + isX.z;
+  i0.yzw = 1.0 - isX;
+//  i0.y += dot( isYZ.xy, vec2( 1.0 ) );
+  i0.y += isYZ.x + isYZ.y;
+  i0.zw += 1.0 - isYZ.xy;
+  i0.z += isYZ.z;
+  i0.w += 1.0 - isYZ.z;
+
+  // i0 now contains the unique values 0,1,2,3 in each channel
+  vec4 i3 = clamp( i0, 0.0, 1.0 );
+  vec4 i2 = clamp( i0-1.0, 0.0, 1.0 );
+  vec4 i1 = clamp( i0-2.0, 0.0, 1.0 );
+
+  //  x0 = x0 - 0.0 + 0.0 * C.xxxx
+  //  x1 = x0 - i1  + 1.0 * C.xxxx
+  //  x2 = x0 - i2  + 2.0 * C.xxxx
+  //  x3 = x0 - i3  + 3.0 * C.xxxx
+  //  x4 = x0 - 1.0 + 4.0 * C.xxxx
+  vec4 x1 = x0 - i1 + C.xxxx;
+  vec4 x2 = x0 - i2 + C.yyyy;
+  vec4 x3 = x0 - i3 + C.zzzz;
+  vec4 x4 = x0 + C.wwww;
+
+// Permutations
+  i = mod289(i);
+  float j0 = permute( permute( permute( permute(i.w) + i.z) + i.y) + i.x);
+  vec4 j1 = permute( permute( permute( permute (
+             i.w + vec4(i1.w, i2.w, i3.w, 1.0 ))
+           + i.z + vec4(i1.z, i2.z, i3.z, 1.0 ))
+           + i.y + vec4(i1.y, i2.y, i3.y, 1.0 ))
+           + i.x + vec4(i1.x, i2.x, i3.x, 1.0 ));
+
+// Gradients: 7x7x6 points over a cube, mapped onto a 4-cross polytope
+// 7*7*6 = 294, which is close to the ring size 17*17 = 289.
+  vec4 ip = vec4(1.0/294.0, 1.0/49.0, 1.0/7.0, 0.0) ;
+
+  vec4 p0 = grad4(j0,   ip);
+  vec4 p1 = grad4(j1.x, ip);
+  vec4 p2 = grad4(j1.y, ip);
+  vec4 p3 = grad4(j1.z, ip);
+  vec4 p4 = grad4(j1.w, ip);
+
+// Normalise gradients
+  vec4 norm = taylorInvSqrt(vec4(dot(p0,p0), dot(p1,p1), dot(p2, p2), dot(p3,p3)));
+  p0 *= norm.x;
+  p1 *= norm.y;
+  p2 *= norm.z;
+  p3 *= norm.w;
+  p4 *= taylorInvSqrt(dot(p4,p4));
+
+// Mix contributions from the five corners
+  vec3 m0 = max(0.6 - vec3(dot(x0,x0), dot(x1,x1), dot(x2,x2)), 0.0);
+  vec2 m1 = max(0.6 - vec2(dot(x3,x3), dot(x4,x4)            ), 0.0);
+  m0 = m0 * m0;
+  m1 = m1 * m1;
+  return 49.0 * ( dot(m0*m0, vec3( dot( p0, x0 ), dot( p1, x1 ), dot( p2, x2 )))
+               + dot(m1*m1, vec2( dot( p3, x3 ), dot( p4, x4 ) ) ) ) ;
+
+  }
+
+uniform float uTime;
+varying vec4 worldPosition;
+float roughness = 0.5;
+
+uniform float metalness;
+// uniform float opacity;
+uniform float opacity;
+#ifdef IOR
+  uniform float ior;
+#endif
+#ifdef USE_SPECULAR
+  uniform float specularIntensity;
+  uniform vec3 specularColor;
+  #ifdef USE_SPECULAR_COLORMAP
+    uniform sampler2D specularColorMap;
+  #endif
+  #ifdef USE_SPECULAR_INTENSITYMAP
+    uniform sampler2D specularIntensityMap;
+  #endif
+#endif
+#ifdef USE_CLEARCOAT
+  uniform float clearcoat;
+  uniform float clearcoatRoughness;
+#endif
+#ifdef USE_DISPERSION
+  uniform float dispersion;
+#endif
+#ifdef USE_IRIDESCENCE
+  uniform float iridescence;
+  uniform float iridescenceIOR;
+  uniform float iridescenceThicknessMinimum;
+  uniform float iridescenceThicknessMaximum;
+#endif
+#ifdef USE_SHEEN
+  uniform vec3 sheenColor;
+  uniform float sheenRoughness;
+  #ifdef USE_SHEEN_COLORMAP
+    uniform sampler2D sheenColorMap;
+  #endif
+  #ifdef USE_SHEEN_ROUGHNESSMAP
+    uniform sampler2D sheenRoughnessMap;
+  #endif
+#endif
+#ifdef USE_ANISOTROPY
+  uniform vec2 anisotropyVector;
+  #ifdef USE_ANISOTROPYMAP
+    uniform sampler2D anisotropyMap;
+  #endif
+#endif
+varying vec3 vViewPosition;
+#include <common>
+#include <packing>
+#include <dithering_pars_fragment>
+#include <color_pars_fragment>
+#include <uv_pars_fragment>
+#include <map_pars_fragment>
+#include <alphamap_pars_fragment>
+#include <alphatest_pars_fragment>
+#include <alphahash_pars_fragment>
+#include <aomap_pars_fragment>
+#include <lightmap_pars_fragment>
+#include <emissivemap_pars_fragment>
+#include <iridescence_fragment>
+#include <cube_uv_reflection_fragment>
+#include <envmap_common_pars_fragment>
+#include <envmap_physical_pars_fragment>
+#include <fog_pars_fragment>
+#include <lights_pars_begin>
+#include <normal_pars_fragment>
+#include <lights_physical_pars_fragment>
+#include <transmission_pars_fragment>
+#include <shadowmap_pars_fragment>
+#include <bumpmap_pars_fragment>
+#include <normalmap_pars_fragment>
+#include <clearcoat_pars_fragment>
+#include <iridescence_pars_fragment>
+#include <roughnessmap_pars_fragment>
+#include <metalnessmap_pars_fragment>
+#include <logdepthbuf_pars_fragment>
+#include <clipping_planes_pars_fragment>
+void main() {
+  // Dynamic roughness for a shimmering ocean effect:
+  float scaleFactor = 50.0;
+  vec4 noiseInput = vec4(vWorldPosition * scaleFactor, uTime * 1.5);
+  roughness = snoise(noiseInput);
+  // roughness = mix(0.3, 0.6, 0.5 * (roughness + 1.0));
+  roughness = mix(0.0, 2.4, 0.5 * (roughness + 1.0));
+
+  vec4 diffuseColor = vec4( diffuse, opacity );
+  #include <clipping_planes_fragment>
+  ReflectedLight reflectedLight = ReflectedLight( vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ) );
+  vec3 totalEmissiveRadiance = emissive;
+  #include <logdepthbuf_fragment>
+  #include <map_fragment>
+  #include <color_fragment>
+  #include <alphamap_fragment>
+  #include <alphatest_fragment>
+  #include <alphahash_fragment>
+  #include <roughnessmap_fragment>
+  #include <metalnessmap_fragment>
+  #include <normal_fragment_begin>
+  #include <normal_fragment_maps>
+  #include <clearcoat_normal_fragment_begin>
+  #include <clearcoat_normal_fragment_maps>
+  #include <emissivemap_fragment>
+  #include <lights_physical_fragment>
+  #include <lights_fragment_begin>
+  #include <lights_fragment_maps>
+  #include <lights_fragment_end>
+  #include <aomap_fragment>
+  vec3 totalDiffuse = reflectedLight.directDiffuse + reflectedLight.indirectDiffuse;
+  vec3 totalSpecular = reflectedLight.directSpecular + reflectedLight.indirectSpecular;
+  #include <transmission_fragment>
+  vec3 outgoingLight = totalDiffuse + totalSpecular + totalEmissiveRadiance;
+  #ifdef USE_SHEEN
+    float sheenEnergyComp = 1.0 - 0.157 * max3( material.sheenColor );
+    outgoingLight = outgoingLight * sheenEnergyComp + sheenSpecularDirect + sheenSpecularIndirect;
+  #endif
+  #ifdef USE_CLEARCOAT
+    float dotNVcc = saturate( dot( geometryClearcoatNormal, geometryViewDir ) );
+    vec3 Fcc = F_Schlick( material.clearcoatF0, material.clearcoatF90, dotNVcc );
+    outgoingLight = outgoingLight * ( 1.0 - material.clearcoat * Fcc ) + ( clearcoatSpecularDirect + clearcoatSpecularIndirect ) * material.clearcoat;
+  #endif
+  #include <opaque_fragment>
+  #include <tonemapping_fragment>
+  #include <colorspace_fragment>
+  #include <fog_fragment>
+  #include <premultiplied_alpha_fragment>
+  #include <dithering_fragment>
+}`;
